d5872a529004766ec401b8f9ae61e572fb284053
Dylan Hillier <dylanslavinhillier@gmail.com>
2023-10-31 10:38:05 +0800

    adds functions for loading the manual images directly

--- example/load_txt_manual_with_captions.py
+++ example/load_txt_manual_with_captions.py
@@ -1,6 +1,9 @@
-import os, json
+"""Example code for interfacing with the atari 2600 manuals."""
+import json
+import os
+import fitz
 
-def load_manual(env_name="MsPacman", path_to_manuals=os.path.join("..", "manuals")) -> str:
+def load_manual_text(env_name="MsPacman", path_to_manuals=os.path.join("..", "manuals")) -> str:
     """
     Load the text manual with the captioned images for a given environment.
     inputs:
@@ -28,7 +31,32 @@ def load_manual(env_name="MsPacman", path_to_manuals=os.path.join("..", "manuals
     # 5. Return the manual
     return manual
 
+def load_manual_images(env_name="MsPacman", path_to_manuals=os.path.join("..", "manuals")) -> list:
+    """
+    Load the text manual with the images for a given environment
+    inputs:
+        env_name (str): The string name of the atari 2600 environment
+        path_to_manuals (str): The path to the manuals directory
+    outputs:
+        images (list): A list of images for the given environment corresponding
+            to the pages of the manual.
+    """
+    path = os.path.join(path_to_manuals, env_name, f"{env_name}.pdf")
+    # 1. Convert the pdf to images
+    doc = fitz.open(path)
+    images = []
+    for page in doc:
+        pix = page.get_pixmap()
+        img = fitz.Pixmap(pix)
+        images.append(img)
+
+    # 2. Return the images
+    return images
 
 
 if __name__ == "__main__":
-    print(load_manual(env_name="Alien"))
+    print(load_manual_text(env_name="Alien", path_to_manuals="manuals"))
+    ## test the pdf to images functionality by saving the images
+    images = load_manual_images(env_name="Alien", path_to_manuals="manuals")
+    for i, image in enumerate(images):
+        image.save(f"Alien_{i}.png")
